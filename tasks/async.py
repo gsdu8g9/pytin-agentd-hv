899d65e0c4423ade450ac780876f0fc2ed143236
servancho <bxtgroup@gmail.com>
2015-11-11 02:51:04 +0300

    shell_hook updated

--- tasks/async.py
+++ tasks/async.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import json
 import os
 import subprocess
 import time
@@ -19,7 +20,11 @@ def shell_hook(hook_name, options):
         raise Exception('Hook script not found: %s' % hook_name)
 
     runtime_dir = os.path.join(os.path.dirname(__file__), 'runtime')
+
+    # save options as json
     json_options_file = os.path.join(runtime_dir, "%s.json" % int(time.time()))
+    with open(json_options_file, mode='w') as json_options:
+        json_options.write(json.dumps(options))
 
     cmd = '/bin/bash %s %s' % (hook_script, json_options_file)
 
@@ -31,14 +36,20 @@ def shell_hook(hook_name, options):
         while True:
             out = process.stdout.readline()
 
+            if out:
+                out = out.decode(encoding='utf-8')
+
             if out == '' and process.poll() is not None:
                 break
             if out != '':
-                shell_hook.update_state(state='PROGRESS', meta={'line': out})
-                command_output.append(out.strip())
+                shell_hook.update_state(state='PROGRESS', meta={'line': out.encode(encoding='utf-8')})
+                command_output.append(out.strip().encode(encoding='utf-8'))
     except Exception, ex:
         process.kill()
         raise ex
+    finally:
+        if os.path.exists(json_options_file):
+            os.remove(json_options_file)
 
     return {
         'output': command_output,
