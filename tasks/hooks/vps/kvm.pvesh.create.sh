0c9806832ecf03088178e18adc12235560c18aed
servancho <bxtgroup@gmail.com>
2016-01-27 19:32:22 +0300

    Typo fix

--- tasks/hooks/vps/kvm.pvesh.create.sh
+++ tasks/hooks/vps/kvm.pvesh.create.sh
@@ -60,7 +60,7 @@ pvesh create /nodes/${NODENAME}/storage/local/content -filename ${DISK_FILE_NAME
 echo "Create VPS"
 pvesh create /nodes/${NODENAME}/qemu -vmid ${VMID} -name ${HOSTNAME} -storage 'local' -memory ${RAM} -sockets 1 -cores ${CPU} -net0 'rtl8139,rate=100,bridge=vmbr0' -virtio0 "local:${VMID}/${DISK_FILE_NAME},cache=writeback,mbps_rd=5,mbps_wr=5" -cdrom "none" -onboot yes
 
-echo "Set config"
+echo "Set args config"
 pvesh set /nodes/${NODENAME}/qemu/${VMID}/config -args "-kernel /root/pyagentd/ipxe.lkrn -append 'dhcp && chain http://${NODENAME}:5000/static/${VMID}.boot.pxe'"
 
 echo "Start VPS install"
